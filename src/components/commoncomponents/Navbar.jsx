2607e31fbb53b6c8a4d3cacb92e72dbc14acecd7
Sabbir Hossain <146484117+sabbirhossain14@users.noreply.github.com>
2025-11-30 23:41:37 +0600

    navbar update

--- src/components/commoncomponents/Navbar.jsx
+++ src/components/commoncomponents/Navbar.jsx
@@ -1,31 +1,55 @@
-import React from 'react'
-import Container from './Container'
-import { Link } from 'react-router-dom'
-import Button from './Button'
+import React from "react";
+import Container from "./Container";
+import { NavLink } from "react-router-dom";
+import Button from "./Button";
+import { motion } from "framer-motion";
+
+const links = [
+  { name: "Home", path: "/" },
+  { name: "About", path: "/about" },
+  { name: "Services", path: "/services" },
+  { name: "Patient Reviews", path: "/patientreviews" },
+  { name: "Contact", path: "/contact" },
+];
+
 
 
 const Navbar = () => {
   return (
-    <Container>
-        <div className="navbar flex items-center text-center justify-between py-4">
-            <div className='w-[] bg-amber-200'>
-                <img src="../public/images/Logo.png" alt="logo" className='w-[173px]'/>
-            </div>
-            <div>
-                <ul className='flex gap-6 font-[Manrope] text-[20px] font-medium text-[#333333]'>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/services">Services</Link></li>
-                    <li><Link to="/patientreviews">Patient Reviews</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
-                </ul>
-            </div>
-            <div>
-                <Button text="Book Now" />
-            </div>
-        </div>
-    </Container>
-  )
-}
+    <div className="bg-[#EBF9FF]">
+      <Container>
+        <div className="navbar flex items-center justify-between py-4">
+          <div>
+            <img src="../public/images/Logo.png" alt="logo" className="w-[173px]" />
+          </div>
 
-export default Navbar
+          <ul className="flex gap-8 font-[Manrope] text-[20px] font-medium text-[#333333] relative">
+            {links.map((link) => (
+              <NavLink key={link.path} to={link.path} className="relative">
+                {({ isActive }) => (
+                  <>
+                    <span className={isActive ? "text-[#29B287]" : ""}>{link.name}</span>
+                    {isActive && (
+                      <motion.div
+                        layoutId="activeUnderline"
+                        className="absolute left-0 right-0 h-[3px] bg-[#29B287] bottom-[-6px]"
+                        transition={{ type: "spring", stiffness: 300, damping: 25 }}
+                      />
+                    )}
+                  </>
+                )}
+              </NavLink>
+            ))}
+            
+          </ul>
+
+          <div>
+            <Button text="Book Now" />
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Navbar;
